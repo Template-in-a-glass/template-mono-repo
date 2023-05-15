9ff497e8a9e063210be3373e161d3bee26374718
Vincent DELACOURT <vdelacou@users.noreply.github.com>
2023-05-15 11:08:15 +0000

    update

--- config/eslintrc.js
+++ config/eslintrc.js
@@ -1,6 +1,27 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const pluginTailwindcss = require("prettier-plugin-tailwindcss");
-
 module.exports = {
-  plugins: [pluginTailwindcss],
-};
+    extends: [
+      "plugin:react/recommended",
+      "airbnb",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@next/next/recommended",
+      "turbo",
+      "prettier",
+    ],
+    plugins: ["react", "@typescript-eslint", "react-hooks"],
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+      "react/react-in-jsx-scope": "off",
+    },
+    parserOptions: {
+      babelOptions: {
+        presets: [require.resolve("next/babel")],
+      },
+    },
+    settings: {
+      react: {
+        version: "18",
+      },
+    },
+  };
+  
