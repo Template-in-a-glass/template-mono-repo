5f28e505686f04c097f41e7495625327ce206bfa
Vincent DELACOURT <vdelacou@users.noreply.github.com>
2023-05-15 11:02:40 +0000

    chore: update settings

--- config/eslintrc.js
+++ config/eslintrc.js
@@ -1,20 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pluginTailwindcss = require("prettier-plugin-tailwindcss");
+
 module.exports = {
-    extends: [
-        "plugin:react/recommended",
-        "airbnb",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:etc/recommended",
-        "plugin:@next/next/recommended",
-        "plugin:unicorn/recommended",
-        "turbo",
-        "prettier"
-    ],
-    rules: {
-        "@next/next/no-html-link-for-pages": "off",
-    },
-    parserOptions: {
-        babelOptions: {
-            presets: [require.resolve("next/babel")],
-        },
-    },
+  plugins: [pluginTailwindcss],
 };
