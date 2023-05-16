0270d09508f3d9f6b9a7e62c92c80b667d4c696b
Vincent DELACOURT <vdelacou@users.noreply.github.com>
2023-05-16 11:12:53 +0800

    Update eslintrc.js

--- config/eslintrc.js
+++ config/eslintrc.js
@@ -21,5 +21,5 @@ module.exports = {
       version: "18",
     },
   },
-  ignorePatterns: ["prettier.config.js", "tailwind.config.js", "commitlint.config.ts"],
+  ignorePatterns: ["prettier.config.js", "tailwind.config.js"],
 };
